b86350379adc1cacb0a35eec9a99bf683aa94230
gustavderdrache <alex.ford@determinate.systems>
2026-03-19 21:04:35 -0400

    formatting

--- tests/custom-bespoke/flake.nix
+++ tests/custom-bespoke/flake.nix
@@ -1,30 +1,25 @@
 {
   description = "Flake with less-structured bespoke outputs";
 
-  inputs = {};
+  inputs = { };
 
-  outputs = _:
-    let
-      
-    in
-    {
-      bespoke.foo = {};
-      bespoke.bar = {};
+  outputs = _: {
+    bespoke.foo = { };
+    bespoke.bar = { };
 
-      schemas.bespoke = {
-        version = 1;
-        doc = ''
-          The `bespoke` flake output exposes a bespoke flake output, such as a library function or code meant to be printed as JSON.
-        '';
-        inventory = _:
-          {
-            children = {
-              foo = {};
-              bar = {
-                shortDescription = "bar contains a description";
-              };
-            };
+    schemas.bespoke = {
+      version = 1;
+      doc = ''
+        The `bespoke` flake output exposes a bespoke flake output, such as a library function or code meant to be printed as JSON.
+      '';
+      inventory = _: {
+        children = {
+          foo = { };
+          bar = {
+            shortDescription = "bar contains a description";
           };
+        };
       };
     };
+  };
 }
